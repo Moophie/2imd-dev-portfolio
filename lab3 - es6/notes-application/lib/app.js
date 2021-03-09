f11c28ef3ba628bea2f8ffa25aaa8ce870f960b6
Moophie <michael.van.lierde@hotmail.com>
2021-03-09 17:13:59 +0100

    Compiled with babel

---
+++ lab3 - es6/notes-application/lib/app.js
@@ -0,0 +1,112 @@
+"use strict";
+
+class Note {
+  constructor(title) {
+    // HINT🤩 this.element = this.createElement(title);
+    this.title = title;
+    this.element = this.createElement(title);
+  }
+
+  createElement(title) {
+    // HINT🤩 newNote.addEventListener('click', this.remove.bind(newNote));
+    let newNote = document.createElement("li");
+    newNote.innerHTML = title;
+    newNote.addEventListener("click", this.remove.bind(newNote));
+    return newNote;
+  }
+
+  add() {
+    // HINT🤩
+    // this function should append the note to the screen somehow
+    let taskList = document.getElementById("taskList");
+    taskList.appendChild(this.element);
+  }
+
+  saveToStorage() {
+    // HINT🤩
+    // localStorage only supports strings, not arrays
+    // if you want to store arrays, look at JSON.parse and JSON.stringify
+    let allNotes = window.localStorage.getItem("allNotes");
+
+    if (allNotes) {
+      allNotes = JSON.parse(allNotes);
+    } else {
+      allNotes = [];
+    }
+
+    allNotes.push(this.title);
+    window.localStorage.setItem("allNotes", JSON.stringify(allNotes));
+  }
+
+  remove() {
+    // HINT🤩 the meaning of 'this' was set by bind() in the createElement function
+    // in this function, 'this' will refer to the current note element
+    // .removeChild(this)
+    // remove the item from screen and from localstorage
+    let taskList = document.getElementById("taskList");
+    taskList.removeChild(this);
+    let allNotes = JSON.parse(window.localStorage.getItem("allNotes"));
+    let index = allNotes.indexOf(this.title);
+    allNotes.splice(index, 1);
+    window.localStorage.setItem("allNotes", JSON.stringify(allNotes));
+  }
+
+}
+
+class App {
+  constructor() {
+    // HINT🤩
+    // pressing the enter key in the text field triggers the createNote function
+    // this.txtTodo = ???
+    // this.txtTodo.addEventListener("keypress", this.createNote.bind(this));
+    // read up on .bind() -> we need to pass the current meaning of this to the eventListener
+    // when the app loads, we can show previously saved noted from localstorage
+    // this.loadNotesFromStorage();
+    console.log("\uD83D\uDC4A\uD83C\uDFFC The Constructor!");
+    this.txtTodo = document.getElementById("taskInput");
+    this.txtTodo.addEventListener("keypress", this.createNote.bind(this));
+    this.loadNotesFromStorage();
+  }
+
+  loadNotesFromStorage() {
+    // HINT🤩
+    // load all notes from storage here and add them to the screen
+    let allNotes = window.localStorage.getItem("allNotes");
+
+    if (allNotes) {
+      allNotes = JSON.parse(allNotes);
+    } else {
+      allNotes = [];
+    }
+
+    allNotes.forEach(n => {
+      let title = n;
+      let note = new Note(title);
+      note.add();
+    });
+  }
+
+  createNote(e) {
+    // this function should create a new note by using the Note() class
+    // HINT🤩
+    // note.add();
+    // note.saveToStorage();
+    // clear the text field with .reset in this class
+    // if (e.key === "Enter")
+    if (e.key === "Enter") {
+      e.preventDefault();
+      let note = new Note(this.txtTodo.value);
+      note.add();
+      note.saveToStorage();
+      this.reset();
+    }
+  }
+
+  reset() {
+    // this function should reset the form / clear the text field
+    this.txtTodo.value = "";
+  }
+
+}
+
+let app = new App();
