9a198be59d0e04a61696738c5256079de19982f6
Moophie <michael.van.lierde@hotmail.com>
2021-05-05 10:06:54 +0200

    build: add basic controller functions for testing

--- lab5 - nodejs/controllers/api/v1/messages.js
+++ lab5 - nodejs/controllers/api/v1/messages.js
@@ -1,7 +1,46 @@
 const getAllMessages = (req, res) => {
-    if(req.query.user){
-        res.json({
-            "message": `GETTING messages`
-        })
-    }
+    res.json({
+        status: "succes",
+        data : {
+            message: `Getting all messages`
+        }
+    })
+}
+
+const getOneMessage = (req, res) => {
+    res.json({
+        status: "succes",
+        data : {
+            message: `Getting message with id`
+        }
+    })
+}
+
+
+const postMessage = (req, res) => {
+    res.json({
+        status: "succes",
+        data : {
+            message: `Posting new message`
+        }
+    })
+}
+
+
+const putMessage = (req, res) => {
+    res.json({
+        status: "succes",
+        data : {
+            message: `Updating message with id`
+        }
+    })
+}
+
+const deleteMessage = (req, res) => {
+    res.json({
+        status: "succes",
+        data : {
+            message: `Deleting message with id`
+        }
+    })
 }
--- lab5 - nodejs/routes/api/v1/messages.js
+++ lab5 - nodejs/routes/api/v1/messages.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const messageController = require('../../../controllers/api/v1/messages');
+var messageController = require('../../../controllers/api/v1/messages');
 
 router.get('/', messageController.getAllMessages);
 router.get('/:id', messageController.getOneMessage);
 
-router.post('/', messageController.post);
-router.put('/:id', messageController.put);
-router.delete('/:id', messageController.delete);
+router.post('/', messageController.postMessage);
+router.put('/:id', messageController.putMessage);
+router.delete('/:id', messageController.deleteMessage);
 
 module.exports = router;
 
