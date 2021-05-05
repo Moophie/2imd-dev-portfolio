a2175c526552c180dadc99583a1debe1937bd600
Moophie <michael.van.lierde@hotmail.com>
2021-05-05 10:35:42 +0200

    feat: index sends get request to api/v1/messages via fetch

--- lab5 - nodejs/routes/index.js
+++ lab5 - nodejs/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require("node-fetch");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  var messages;
+
+  fetch('http://localhost:3000/api/v1/messages/')
+    .then(response => response.json())
+    .then(data => $messages = data);
+
+  res.render('index', { title: 'MessageAPI', messages: $messages });
 });
 
 module.exports = router;
