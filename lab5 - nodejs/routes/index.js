55a7f85eabd73739d76a19d8252d2aa82d5254b4
Moophie <michael.van.lierde@hotmail.com>
2021-05-05 11:58:13 +0200

    fix: catch error on fetch request

--- lab5 - nodejs/routes/index.js
+++ lab5 - nodejs/routes/index.js
@@ -4,11 +4,14 @@ const fetch = require("node-fetch");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  fetch('/api/v1/messages/')
+  fetch('/api/v1/messages')
     .then(response => response.json())
     .then(data => {
       $messages = data;
       res.render('index', { title: 'MessageAPI', messages: $messages })
+    })
+    .catch((error) => {
+      console.error('Error:', error);
     });
 });
 
