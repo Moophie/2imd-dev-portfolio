58b1f0bdf66818313ba8020067c22c1ac5249ed2
Moophie <michael.van.lierde@hotmail.com>
2021-05-05 11:21:34 +0200

    fix: correct path in index fetch request

--- lab5 - nodejs/routes/index.js
+++ lab5 - nodejs/routes/index.js
@@ -4,9 +4,7 @@ const fetch = require("node-fetch");
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var messages;
-
-  fetch('http://localhost:3000/api/v1/messages/')
+  fetch('api/v1/messages/')
     .then(response => response.json())
     .then(data => $messages = data);
 
